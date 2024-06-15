628cc8ffb2897ff53e231d03be2a8feb73536cf2
Anis Mhamid <anesmhamed1@gmail.com>
2024-06-15 19:25:58 +0300

    updates on leason-5-4

--- works/Leason5/T4my/gallery.js
+++ works/Leason5/T4my/gallery.js
@@ -1,63 +1,32 @@
 let images = [
-    '1.jpg',
-    '3.jpg',
-    '6.jpg',
-    '8.jpg'
-]
-
-let box = document.querySelector('#box');
+    '../works/Leason5/T4my/1.jpeg',
+    '../works/Leason5/T4my/2.jpeg',
+    '../works/Leason5/T4my/3.jpeg',
+    '../works/Leason5/T4my/4.jpeg'
+];
+let mainImage = document.getElementById('main-pic');
 let imageIndex = 0;
 
-box.innerHTML += `
-<div style="width:626px;height:500px;">
-<img id="mainImage" style="width:100%;display: block;" src="${images[imageIndex]}"></img>
-</div>
-`;
-
-box.innerHTML += `<div style="display:flex;">`;
-
-for (let i = 0; i < images.length; i++) {
-    box.innerHTML += `
-    <img style="width:150px;height: 150px;object-fit: fill;
-    margin: 5px 2px;"
-    src="${images[i]}">
-    </img>
-    `;
-}
-box.innerHTML += `</div>`;
-
-box.innerHTML += ` 
-    <div style="width:600px;display: flex; justify-content: space-between; margin-top: 10px;">
-        <button type='button' onclick="backImage()" id='back'>Back</button>
-        <button type='button' onclick="nextImage()" id='next'>Next</button>
-    </div>
-`;
-
-
-
-function backImage() {
+function back() {
     if (imageIndex > 0) {
         imageIndex--;
-        updateImage();
     } else {
-        imageIndex = images.length -1;
-        updateImage();
+        imageIndex = images.length - 1;
     }
+    updateGallery();
 }
-function nextImage() {
+
+function next() {
     if (imageIndex < images.length - 1) {
         imageIndex++;
-        updateImage();
     } else {
-        imageIndex = 0; 
-        updateImage();
+        imageIndex = 0;
     }
+    updateGallery();
 }
 
-function updateImage() {
-    let mainImage = document.querySelector('#mainImage');
+function updateGallery() {
     mainImage.src = images[imageIndex];
 }
-
 
 
