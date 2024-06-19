947beba318453a46c4ee6a5eab3b7790f2f538d4
Anis Mhamid <anesmhamed1@gmail.com>
2024-06-19 14:51:52 +0300

    updates leason - 8

--- works/Leason3/D/js/targil4.js
+++ works/Leason3/D/js/targil4.js
@@ -2,13 +2,13 @@ function chagedFlag() {
     var value = document.getElementById('userValue').value;
     switch (value) {
         case 'swiss':
-            document.getElementById('img').src = './works/Leason3/D/swiss.jpg'
+            document.getElementById('img').src = '../works/Leason3/D/swiss.jpg'
             break;
         case 'tunisia':
-            document.getElementById('img').src = './works/Leason3/D/Flag_of_Tunisia.svg.png'
+            document.getElementById('img').src = '../works/Leason3/D/Flag_of_Tunisia.svg.png'
             break;
         default:
-            document.getElementById('img').src = './works/Leason3/D/Flag_of_Tunisia.svg.png'
+            document.getElementById('img').src = '../works/Leason3/D/Flag_of_Tunisia.svg.png'
             break;
     }
 }
